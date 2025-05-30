ebb7cf7d946cb0c6be074068e4d59776a71b4b4e
Lyle Menard <lylemenard833@gmail.com>
2025-05-30 11:06:56 -0500

    refactored initial scene, scene properly styled to take up view of main window and resize

--- src/lib/FieldPlot.svelte
+++ src/lib/FieldPlot.svelte
@@ -1,56 +1,105 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
 	import * as THREE from 'three';
-	import field from '$lib/assets/field.png';
+	import fieldTexture from '$lib/assets/field.png';
 
-	if (browser) {
-		let camera: THREE.PerspectiveCamera;
-		let scene: THREE.Scene;
-		let renderer: THREE.WebGLRenderer;
-		let plane: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
+	type Field = {
+		HomeTeam: string;
+		AwayTeam: string;
+		Mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap>;
+	};
 
-		const init = () => {
-			scene = new THREE.Scene();
-			camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	let threeJSContainer: HTMLElement;
+	let renderer: THREE.WebGLRenderer;
+	let scene: THREE.Scene;
+	let camera: THREE.PerspectiveCamera;
+	let initialized: boolean = false;
+	let meshes: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>[] = [];
 
-			renderer = new THREE.WebGLRenderer();
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			document.body.appendChild(renderer.domElement);
+	onMount(() => {
+		if (browser && !initialized) {
+			initScene(window.innerWidth, window.innerHeight);
+			initModels();
+		}
+	});
 
-			const loader = new THREE.TextureLoader();
-			const texture = loader.load(field);
-			texture.colorSpace = THREE.SRGBColorSpace;
+	const initScene = (width: number, height: number) => {
+		// initialize renderer and append to DOM
+		renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.setSize(width, height);
+		threeJSContainer.appendChild(renderer.domElement);
 
-			const geometery = new THREE.PlaneGeometry(1, 1);
-			const material = new THREE.MeshBasicMaterial({ map: texture });
+		scene = new THREE.Scene();
+		camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+		camera.position.z = 5;
 
-			plane = new THREE.Mesh(geometery, material);
-			scene.add(plane);
+		renderer.render(scene, camera);
+		renderer.setAnimationLoop(animate);
 
-			camera.position.z = 5;
-		};
+		window.addEventListener('resize', handleResize);
+		initialized = true;
+	};
 
-		const render = () => {
-			renderer.clear();
-			renderer.render(scene, camera);
-		};
+	const animate = () => {
+		meshes.forEach((mesh) => {
+			mesh.rotation.x += 0.005;
+			mesh.rotation.y += 0.005;
+		});
 
-		const animate = () => {
-			requestAnimationFrame(animate);
+		render();
+	};
 
-			plane.rotation.x += 0.005;
-			plane.rotation.y += 0.005;
+	const render = () => {
+		renderer.clear();
+		renderer.render(scene, camera);
+	};
 
-			render();
-		};
+	const handleResize = () => {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	};
 
-		init();
-		animate();
-	}
+	const initModels = () => {
+		const testField = createField('home', 'away');
+		$inspect(testField);
+
+		scene.add(testField);
+		meshes.push(testField);
+	};
+
+	const createField = (home: string, away: string) => {
+		const geometery = new THREE.PlaneGeometry(1, 1);
+		const loader = new THREE.TextureLoader();
+
+		const texture = loader.load(fieldTexture);
+		texture.colorSpace = THREE.SRGBColorSpace;
+		texture.magFilter = THREE.NearestFilter;
+		texture.minFilter = THREE.NearestFilter;
+
+		const material = new THREE.MeshBasicMaterial({
+			map: texture
+		});
+		const plane = new THREE.Mesh(geometery, material);
+		// const newField: Field = { home, away, plane };
+		return plane;
+	};
 </script>
 
 <svelte:head>
 	<title>SvelteKit + ThreeJS</title>
 </svelte:head>
 
-<section>Hello World</section>
+<div class="threeJSCanvas" bind:this={threeJSContainer}></div>
+
+<style>
+	div.threeJSCanvas {
+		display: block;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,22 @@
 </script>
 
 <FieldPlot />
+
+<style>
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+	}
+	canvas {
+		display: block;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
